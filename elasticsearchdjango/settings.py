f505d2b333829a90689de70296314303d41d6d8c
김민중 <kl4314@likelion.org>
2018-01-08 15:10:16 +0900

    장고와 엘라스틱 서치 연동

--- elasticsearchdjango/settings.py
+++ elasticsearchdjango/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'elasticsearchdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elasticsearchdb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
