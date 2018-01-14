7ceccac5c8b3a10c670d8c5ae794241538df7cba
김민중 <kl4314@likelion.org>
2018-01-14 22:09:33 +0900

    로그아웃 할 때 400 에러가 뜨는 오류 수정

--- polls/views.py
+++ polls/views.py
@@ -224,32 +224,9 @@ def signin(request):
 
 def signout(request):
 
-    if request.method == 'GET':
-        del request.session['userid']
+    del request.session['userid']
 
-        client_id = "DshukL7WQcANLYUiQTsY"
-        client_secret = "p5RxLlzjyJ"
-
-        q = request.GET.get('q')
- 
-        encText = urllib.parse.quote("{}".format(q))
-        url = "https://openapi.naver.com/v1/search/shopping?query=" + encText #json 결과
-        movie_api_request = urllib.request.Request(url)
-        movie_api_request.add_header("X-Naver-Client-Id", client_id)
-        movie_api_request.add_header("X-Naver-Client-Secret", client_secret)
-        response = urllib.request.urlopen(movie_api_request)
-        rescode = response.getcode()
-
-        if (rescode == 200):
-            response_body = response.read()
-            result = json.loads(response_body.decode('utf-8'))
-            items = result.get('items')
-
-            context = {
-                'items':items
-            }
-      
-            return render(request, 'search/searchproduct.html', context=context)
+    return render(request, 'search/searchproduct.html')
 
 
 
